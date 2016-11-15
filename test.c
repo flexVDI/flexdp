db928ed6fd157188f7ca2bdd76aa9f885099d8dc
Javier Celaya <javier.celaya@flexvdi.com>
2016-11-15 13:29:40 +0100

    Add capabilities message test

--- test.c
+++ test.c
@@ -1,7 +1,23 @@
+#include <stdio.h>
 #define FLEXVDI_PROTO_IMPL
 #include "flexdp.h"
 
+#define CHECK(cond) \
+    do if (!(cond)) { \
+       fprintf(stderr, "%s:%d: assertion '" #cond "' failed\n", __FUNCTION__, __LINE__); \
+       return 1; \
+    } while(0)
+
+int test_FlexVDICapabilitiesMsg() {
+    FlexVDICapabilitiesMsg msg = {0};
+    CHECK(!supportsCapability(&msg, FLEXVDI_CAP_PRINTING));
+    setCapability(&msg, FLEXVDI_CAP_PRINTING);
+    CHECK(supportsCapability(&msg, FLEXVDI_CAP_PRINTING));
+    resetCapability(&msg, FLEXVDI_CAP_PRINTING);
+    CHECK(!supportsCapability(&msg, FLEXVDI_CAP_PRINTING));
+}
+
 int main() {
-    return 0;
+    return test_FlexVDICapabilitiesMsg();
 }
 
