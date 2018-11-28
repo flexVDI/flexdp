a412e5d313c5885d59b780cdb0cc7e643355d99f
Javier Celaya <jcelaya@gmail.com>
2018-11-28 19:35:07 +0100

    Add GPL3 header

--- flexdp.h
+++ flexdp.h
@@ -1,6 +1,21 @@
-/**
- * Copyright Flexible Software Solutions S.L. 2014
- **/
+/*
+    Copyright (C) 2014-2018 Flexible Software Solutions S.L.U.
+
+    This file is part of flexDP.
+
+    flexDP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    flexDP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with flexDP. If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef _FLEXVDIPROTO_H_
 #define _FLEXVDIPROTO_H_
